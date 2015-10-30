043723640ba7cc77146f7d1b6cfbde93dcb025d4
Alicia Beltran <aliceresponde@gmail.com>
2015-10-30 17:02:14 -0500

    Frist Screen

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label_recording: UILabel!
+    
+    @IBOutlet weak var btn_stop: UIButton!
+    
+    @IBOutlet weak var btn_recording: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        //hidde stop_btn
+        self.btn_stop.hidden=true;
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+   
+    @IBAction func press_recording_btn(sender: UIButton) {
+        
+        // print 
+        print("clicked")
+        //meke visble the label
+        self.label_recording.hidden=false
+        self.btn_stop.hidden=false
+        self.btn_recording.enabled=false
+        
+    }
+  
+    @IBAction func press_stop_recording(sender: UIButton) {
+        //hidde label
+        self.label_recording.hidden=true
+        self.btn_stop.hidden=true
+        self.btn_recording.enabled=true
+    }
+   
 
 }
 
